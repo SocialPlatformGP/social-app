81d028c7e9523f441fcbea30b42c8b209593b34a
Edrees <mohamededrees234@hotmail.com>
2023-10-09 21:36:09 +0300

    created CreatePostFragment and viewmodel

---
+++ feature/posts/src/main/java/com/gp/socialapp/presentation/createpost/CreatePostFragment.kt
@@ -0,0 +1,20 @@
+package com.gp.socialapp.presentation.createpost
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gp.posts.R
+
+class CreatePostFragment : Fragment() {
+    private lateinit var viewModel: CreatePostViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_post, container, false)
+    }
+}
---
+++ feature/posts/src/main/java/com/gp/socialapp/presentation/createpost/CreatePostViewModel.kt
@@ -0,0 +1,7 @@
+package com.gp.socialapp.presentation.createpost
+
+import androidx.lifecycle.ViewModel
+
+class CreatePostViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
