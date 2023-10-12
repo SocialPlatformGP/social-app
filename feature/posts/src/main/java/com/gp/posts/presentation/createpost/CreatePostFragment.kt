f0ae3321dae865528e9e974f3f93aac4e5d64561
Edrees <mohamededrees234@hotmail.com>
2023-10-13 00:13:06 +0300

    some minor bug fixes

--- feature/posts/src/main/java/com/gp/posts/presentation/createpost/CreatePostFragment.kt
+++ feature/posts/src/main/java/com/gp/posts/presentation/createpost/CreatePostFragment.kt
@@ -1,5 +1,6 @@
 package com.gp.posts.presentation.createpost
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,18 +8,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.gp.posts.R
 import com.gp.posts.databinding.FragmentCreatePostBinding
 import com.gp.socialapp.utils.State
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -29,7 +36,8 @@ class CreatePostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=DataBindingUtil.inflate(layoutInflater,R.layout.fragment_create_post,container,false)
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_create_post, container, false)
         binding.lifecycleOwner = this
         binding.viewmodel = viewModel
         return binding.root
@@ -37,33 +45,58 @@ class CreatePostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        addCreatedStateCollector()
+        addCancelPressedCollector()
+        //TODO("implement visiblity option dropdown menu")
+    }
+    private fun addCreatedStateCollector(){
         lifecycleScope.launch {
-            viewModel.uiState.flowWithLifecycle(lifecycle).collect {uiState ->
-                Log.d("TAG", "1- Success triggerd in collector")
+            viewModel.uiState.flowWithLifecycle(lifecycle).distinctUntilChanged { old, new ->
+                old.createdState == new.createdState
+            }.collect { uiState ->
                 when (uiState.createdState) {
                     is State.Success -> {
-                        Log.d("TAG", "2- Success triggerd in collector")
                         findNavController().navigate(R.id.action_createPostFragment_to_feedFragment)
-                        Toast.makeText(
-                            context,
+                        makeSnackbar(
                             getString(R.string.post_created_successfully),
-                            Toast.LENGTH_LONG
-                        ).show()
+                            Snackbar.LENGTH_LONG
+                        )
                     }
                     is State.Error -> {
-                        Log.e("TAG", (uiState.createdState as State.Error).message)
-                        Toast.makeText(
-                            context,
+                        makeSnackbar(
                             (uiState.createdState as State.Error).message,
-                            Toast.LENGTH_LONG
-                        ).show()
-                        TODO("Provide an informative error message to the user")
+                            Snackbar.LENGTH_LONG
+                        )
+                        //TODO("Provide an informative error message to the user")
                     }
-                    else -> {
-                        Log.d("Edrees", "I am stupid")
-                    }
+                    else ->{}
                 }
             }
         }
+    }
+    private fun addCancelPressedCollector(){
+        lifecycleScope.launch {
+            viewModel.uiState.flowWithLifecycle(lifecycle).distinctUntilChanged { old, new ->
+                old.cancelPressed == new.cancelPressed
+            }.collect { uiState ->
+                if (uiState.cancelPressed) {
+                    MaterialAlertDialogBuilder(requireContext()).setTitle("Discard post draft?")
+                        .setPositiveButton("Discard") { dialog, which ->
+                            findNavController().navigate(R.id.action_createPostFragment_to_feedFragment)
+                        }.setNegativeButton("Cancel"){_, _ ->
+                        }.show()
+                    viewModel.resetCancelPressed()
+                }
+            }
+        }
+    }
+
+    private fun makeSnackbar(message: String, duration: Int) {
+        Snackbar.make(
+            requireContext(),
+            binding.root,
+            message,
+            duration
+        ).show()
     }
 }
--- feature/posts/src/main/java/com/gp/posts/presentation/createpost/CreatePostViewModel.kt
+++ feature/posts/src/main/java/com/gp/posts/presentation/createpost/CreatePostViewModel.kt
@@ -1,41 +1,51 @@
 package com.gp.posts.presentation.createpost
 
-import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.users.model.User
 import com.gp.socialapp.model.Post
 import com.gp.socialapp.repository.PostRepository
-import com.gp.socialapp.utils.State
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import javax.inject.Inject
+
 @HiltViewModel
 class CreatePostViewModel @Inject constructor (
     private val postRepository: PostRepository
 ) : ViewModel() {
     val uiState = MutableStateFlow(CreatePostUIState())
+    private val currentUser = User("Mohammed Edrees",
+        "https://upload.wikimedia.org/wikipedia/commons/b/b3/Solid_gray.png",
+    "mohamed@edrees.com")
     private val currentUserName = "User 1"
+    init{
+        uiState.value.userProfilePicURL = currentUser.profilePictureURL
+    }
     @RequiresApi(Build.VERSION_CODES.O)
     fun onCreatePost(){
         viewModelScope.launch {
             with(uiState.value) {
-                Log.d("EDREES", "OnCreate Called")
                 val state =
                     postRepository.createPost(Post(currentUserName, LocalDateTime.now(), title, body, 0, 0, false))
                 state.collect{newState ->
-                    uiState.update {
-                        it.copy(createdState = newState)
-                    }
-                    Log.d("EDREES", "OnCreate Executed: ${newState}, UiState: ${uiState.value.createdState}")
+                    uiState.value = uiState.value.copy(createdState = newState)
                 }
             }
+            delay(500)
         }
+    }
+    fun onCancel(){
+        uiState.value = uiState.value.copy(cancelPressed = true)
+    }
+
+    fun resetCancelPressed() {
+        uiState.value.cancelPressed = false
     }
 }
