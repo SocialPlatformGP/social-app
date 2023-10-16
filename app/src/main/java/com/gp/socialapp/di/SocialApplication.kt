bd488a69928dda39a7e37188c815b953513d209e
Edrees <muhammed.edrees101@gmail.com>
2023-10-16 22:53:05 +0300

    Revert "Feature posts"

--- app/src/main/java/com/gp/socialapp/di/SocialApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.gp.socialapp.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SocialApplication: Application() {
-}
--- data/users/src/main/java/com/example/users/model/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.users.model
-
-data class User(
-    val name: String,
-    val profilePictureURL: String,
-    val email: String,
-)
