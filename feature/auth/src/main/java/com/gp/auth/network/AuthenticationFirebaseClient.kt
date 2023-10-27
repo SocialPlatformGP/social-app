65fd8bd8752ed1ead4ed22eb9bf1f981fe04577c
Mohamed Zarea <mohamedzarea29@gmail.com>
2023-10-27 23:05:14 +0200

    fixed bug

--- feature/auth/src/main/java/com/gp/auth/network/AuthenticationFirebaseClient.kt
+++ feature/auth/src/main/java/com/gp/auth/network/AuthenticationFirebaseClient.kt
@@ -1,28 +1,17 @@
 package com.gp.auth.network
 
-import android.content.Context
-import android.content.Intent
-import android.content.IntentSender
 import android.util.Log
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat.startActivity
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
-import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import com.gp.auth.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.gp.socialapp.utils.State
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.tasks.await
-import java.util.concurrent.CancellationException
 import javax.inject.Inject
 
 class AuthenticationFirebaseClient @Inject constructor(private val auth: FirebaseAuth): AuthenticationRemoteDataSource {
@@ -79,4 +68,3 @@ class AuthenticationFirebaseClient @Inject constructor(private val auth: Firebas
     }
 }
 
-//}
