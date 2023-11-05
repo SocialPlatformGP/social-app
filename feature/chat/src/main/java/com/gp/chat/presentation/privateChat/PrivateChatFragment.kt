e23cc5299f74089ebf97020f187c79b1e4a05923
Mohamed Zarea <mohamedzarea29@gmail.com>
2023-11-05 16:56:44 +0200

    issue fixed

--- feature/chat/src/main/java/com/gp/chat/presentation/privateChat/PrivateChatFragment.kt
+++ feature/chat/src/main/java/com/gp/chat/presentation/privateChat/PrivateChatFragment.kt
@@ -3,6 +3,7 @@ package com.gp.chat.presentation.privateChat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.net.Uri
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import androidx.databinding.DataBindingUtil
@@ -14,6 +15,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.gp.chat.R
 import com.gp.chat.adapter.MessageAdapter
 import com.gp.chat.databinding.FragmentPrivateChatBinding
+import com.gp.chat.listener.MyOpenDocumentContract
+import com.gp.chat.listener.MyScrollToBottomObserver
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -21,7 +24,10 @@ import kotlinx.coroutines.launch
 class PrivateChatFragment : Fragment() {
     lateinit var binding: FragmentPrivateChatBinding
     private val viewModel: PrivateChatViewModel by viewModels()
-    override fun onCreateView(
+
+
+
+        override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -40,11 +46,28 @@ class PrivateChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = binding.recyclerMessage
+        val manager = LinearLayoutManager(requireContext())
+        manager.stackFromEnd = true
+        recyclerView.layoutManager = manager
         val adapter = MessageAdapter()
+        adapter.registerAdapterDataObserver(
+            MyScrollToBottomObserver(
+                recyclerView,
+                adapter,
+                manager
+            )
+        )
         recyclerView.adapter = adapter
         lifecycleScope.launch {
             viewModel.messages.flowWithLifecycle(lifecycle).collect{
                 adapter.submitList(it)
+                recyclerView.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+                    override fun onPreDraw(): Boolean {
+                        recyclerView.viewTreeObserver.removeOnPreDrawListener(this)
+                        recyclerView.scrollToPosition(adapter.itemCount - 1)
+                        return true
+                    }
+                })
             }
         }
     }
