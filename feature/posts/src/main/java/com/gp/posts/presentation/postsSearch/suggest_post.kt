850c93312bfdf590be440c18db8c6826832403c1
Edrees <mohamededrees234@hotmail.com>
2023-11-10 19:24:37 +0200

    fixed some bugs

--- feature/posts/src/main/java/com/gp/posts/presentation/postsSearch/suggest_post.kt
+++ feature/posts/src/main/java/com/gp/posts/presentation/postsSearch/suggest_post.kt
@@ -1,101 +1,73 @@
 package com.gp.posts
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.navArgs
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.button.MaterialButton
-import com.gp.posts.adapter.FeedPostAdapter
 import com.gp.posts.adapter.SuggestPostAdapter
-import com.gp.posts.databinding.FragmentSearchBinding
 import com.gp.posts.databinding.FragmentSuggestPostBinding
-import com.gp.posts.listeners.OnMoreOptionClicked
-import com.gp.posts.listeners.VotesClickedListener
+import com.gp.posts.listeners.OnSuggestedPostClickListener
 import com.gp.posts.presentation.postsSearch.SearchViewModel
-import com.gp.socialapp.database.model.PostEntity
-import com.gp.socialapp.database.model.ReplyEntity
+import com.gp.socialapp.model.Post
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [suggest_post.newInstance] factory method to
- * create an instance of this fragment.
- */
 @AndroidEntryPoint
-class suggest_post : Fragment() {
+class suggest_post : Fragment(), OnSuggestedPostClickListener {
     private val viewModel: SearchViewModel by viewModels()
     lateinit var binding: FragmentSuggestPostBinding
-    lateinit var btnSearch: Button
-    var searchText :String=""
-
+    var searchText: String = ""
 
 
     fun updateSearchQuery(query: String?) {
-        searchText=query!!
+        searchText = query!!
 
-        if(query.isNullOrEmpty()){
+        if (query.isNullOrEmpty()) {
             binding.rvSuggestPosts.visibility = View.GONE
-        }
-        else{
+        } else {
             binding.rvSuggestPosts.visibility = View.VISIBLE
-            viewModel.searchPosts(query)
+            viewModel.searchPostsByTitle(query)
         }
     }
+
     fun navigateToFinalResult(query: String?) {
-        val action=suggest_postDirections.actionSuggestPostToSearchFragment2(query!!)
+        val action = suggest_postDirections.actionSuggestPostToSearchFragment2(query!!)
         Navigation.findNavController(requireView()).navigate(action)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_suggest_post, container, false)
-        val view = binding.root
-
-        val btnSearch = view.findViewById<Button>(R.id.btn_search)
-        btnSearch.setOnClickListener {
-            val action=suggest_postDirections.actionSuggestPostToSearchFragment2(searchText)
-            Navigation.findNavController(view).navigate(action)
-
-        }
-
-        return view
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_suggest_post, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = binding.rvSuggestPosts
-        val adapter = SuggestPostAdapter()
+        val adapter = SuggestPostAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         lifecycleScope.launch {
             viewModel.searchResult.flowWithLifecycle(lifecycle).collect {
-
                 binding.rvSuggestPosts.visibility = View.VISIBLE
                 adapter.submitList(it)
-
             }
         }
-
-
-
     }
+
+    override fun onClick(model: Post) {
+        val action = suggest_postDirections.actionSuggestPostToSearchFragment2(model.title, false)
+        findNavController().navigate(action)
     }
+}
