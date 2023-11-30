8157f4bc9db2dc8cf949b2fd5dcbf721a672ec96
Edrees <mohamededrees234@hotmail.com>
2023-11-30 20:04:49 +0200

    added tag filters to feed options

--- feature/posts/src/main/java/com/gp/posts/presentation/postsfeed/FeedFragment.kt
+++ feature/posts/src/main/java/com/gp/posts/presentation/postsfeed/FeedFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import androidx.annotation.RequiresApi
+import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -19,6 +20,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.chip.Chip
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
